7a5d0b45c44c05b7b55ca6d711ea268b2eeb9114
Azerus <kevin.marous@group-hpi.com>
2018-11-26 21:41:55 +0100

    fix middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,7 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check() && Auth::user()->role->id == 1) {
+        if (Auth::guard($guard)->check() && Auth::user()->role_id == 1) {
             return redirect()->route('admin.dashboard');
         } elseif (Auth::guard($guard)->check() && Auth::user()->role->id == 2)
         {
