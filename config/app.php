2a175e10b0668dbcb880e7072379f8d8faa89aed
Azerus <kevin.marous@group-hpi.com>
2018-11-26 15:16:23 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        'Kamaln7\Toastr\ToastrServiceProvider',
+        Kamaln7\Toastr\ToastrServiceProvider,
 
     ],
 
@@ -206,7 +206,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
-        'Toastr' => 'Kamaln7\Toastr\Facades\Toastr',
+        'Toastr' => Kamaln7\Toastr\Facades\Toastr,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
